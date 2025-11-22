8f91cd6d265e63ec771ff5297307433772e971c0
Julio Fernandez <jfvilas@outlook.com>
2025-11-22 20:51:00 +0100

    date item count

--- frontend/src/FileManager/FileList/FileList.jsx
+++ frontend/src/FileManager/FileList/FileList.jsx
@@ -11,161 +11,159 @@ import { useTranslation } from "../../contexts/TranslationProvider";
 import "./FileList.scss";
 
 const FileList = ({
-  actions,
-  icons,
-  filter,
-  onCreateFolder,
-  onRename,
-  onFileOpen,
-  onRefresh,
-  enableFilePreview,
-  triggerAction,
-  permissions,
-  formatDate
+    actions,
+    icons,
+    filter,
+    onCreateFolder,
+    onRename,
+    onFileOpen,
+    onRefresh,
+    enableFilePreview,
+    triggerAction,
+    permissions,
+    formatDate
 }) => {
-  const { currentPathFiles, sortConfig, setSortConfig } = useFileNavigation();
-  const filesViewRef = useRef(null)
-  const { activeLayout } = useLayout()
-  const t = useTranslation()
-  const { options } = useOptions()
+    const { currentPathFiles, sortConfig, setSortConfig } = useFileNavigation();
+    const filesViewRef = useRef(null)
+    const { activeLayout } = useLayout()
+    const t = useTranslation()
+    const { options } = useOptions()
 
-  const {
-    emptySelecCtxItems,
-    selecCtxItems,
-    handleContextMenu,
-    unselectFiles,
-    visible,
-    setVisible,
-    setLastSelectedFile,
-    selectedFileIndexes,
-    clickPosition,
-    isSelectionCtx,
-  } = useFileList(onRefresh, enableFilePreview, triggerAction, permissions, onFileOpen)
+    const {
+        emptySelecCtxItems,
+        selecCtxItems,
+        handleContextMenu,
+        unselectFiles,
+        visible,
+        setVisible,
+        setLastSelectedFile,
+        selectedFileIndexes,
+        clickPosition,
+        isSelectionCtx,
+    } = useFileList(onRefresh, enableFilePreview, triggerAction, permissions, onFileOpen)
 
-  const contextMenuRef = useDetectOutsideClick(() => setVisible(false))
+    const contextMenuRef = useDetectOutsideClick(() => setVisible(false))
 
-  const handleSort = (key) => {
-    let direction = "asc"
-    if (sortConfig.key === key && sortConfig.direction === "asc") direction = "desc"
-    setSortConfig({ key, direction })
-  }
+    const handleSort = (key) => {
+        let direction = "asc"
+        if (sortConfig.key === key && sortConfig.direction === "asc") direction = "desc"
+        setSortConfig({ key, direction })
+    }
 
-  const customActionClick = (src)=> {
-    setVisible(false)
-    if (actions) {
-      for (let actionKey of actions.keys()) {
-        let allSameClass = (selectedFileIndexes.length>0) && selectedFileIndexes.every(i => currentPathFiles[i].class===actionKey)
-        if (allSameClass) {
-          for (let customAction of actions.get(actionKey)) {
-            if (customAction.title === src.target.innerText) {
-              let selectedFiles=currentPathFiles.filter ( (f,i) => selectedFileIndexes.includes(i))
-              customAction.onClick(selectedFiles)
+    const customActionClick = (src)=> {
+        setVisible(false)
+        if (actions) {
+        for (let actionKey of actions.keys()) {
+            let allSameClass = (selectedFileIndexes.length>0) && selectedFileIndexes.every(i => currentPathFiles[i].class===actionKey)
+            if (allSameClass) {
+            for (let customAction of actions.get(actionKey)) {
+                if (customAction.title === src.target.innerText) {
+                let selectedFiles=currentPathFiles.filter ( (f,i) => selectedFileIndexes.includes(i))
+                customAction.onClick(selectedFiles)
+                }
             }
-          }
+            }
         }
-      }
-    }
-  }
-
-  let contextItems = selecCtxItems
-  if (actions) {
-    for (let actionKey of actions.keys()) {
-      let allSameClass = (selectedFileIndexes.length>0) && selectedFileIndexes.every(i => currentPathFiles[i].class===actionKey)
-      if (allSameClass) {
-        contextItems[contextItems.length-1].divider=true
-        for (let customAction of actions.get(actionKey)) {
-          contextItems.push({
-            title:customAction.title,
-            icon:customAction.icon,
-            divider:customAction.divider,
-            onClick:customActionClick
-          })
         }
-      }
-    }
-  }
-
-  const renderStatusBar = () => {
-    if (!options.statusBar) return <></>
-
-    let text=currentPathFiles.length + ' Items'
-    if (selectedFileIndexes.length>0) {
-      text+= '\u00A0\u00A0\u00A0\u00A0' + selectedFileIndexes.length +'\u00A0items selected'
-      let size = currentPathFiles.filter ((f,i) => selectedFileIndexes.includes(i)).reduce((a,c) => {
-        return (c.size||0)+a
-      },0)
-      var units = 'bytes'
-      if (size>1024) {
-        size = (size/1024).toFixed(0)
-        units='KB'
-        if (size >1024) {
-          size = (size/1024).toFixed(0)
-          units='MB'
-        }
-      }
-      text+= '\u00A0\u00A0\u00A0\u00A0' + size + '\u00A0' + units
     }
 
-    return (
-      <div className="statusbar" style={{...(!options.folderTree? {borderBottomLeftRadius: '8px'} : {})}}>{text}</div>
-    )
-  }
+    let contextItems = selecCtxItems
+    if (actions) {
+        for (let actionKey of actions.keys()) {
+            let allSameClass = (selectedFileIndexes.length>0) && selectedFileIndexes.every(i => currentPathFiles[i].class===actionKey)
+            if (allSameClass) {
+                contextItems[contextItems.length-1].divider=true
+                for (let customAction of actions.get(actionKey)) {
+                    contextItems.push({
+                        title:customAction.title,
+                        icon:customAction.icon,
+                        divider:customAction.divider,
+                        onClick:customActionClick
+                    })
+                }
+            }
+        }
+    }
 
-  return (
-    <>
-      <div
-        ref={filesViewRef}
-        className={`files ${activeLayout}`}
-        style={{height: (options.statusBar? 'calc(100% - (54px))':'calc(100% - (35px))'), borderBottom: (options.statusBar?1:0)}}
-        onContextMenu={handleContextMenu}
-        onClick={unselectFiles}
-      >
-        {activeLayout === "list" && (
-          <FilesHeader unselectFiles={unselectFiles} onSort={handleSort} sortConfig={sortConfig} />
-        )}
+    const renderStatusBar = () => {
+        if (!options.statusBar) return <></>
 
-        {currentPathFiles?.length > 0 ? (
-          <>
-            {currentPathFiles.map((file, index) => (
-              file.name.toLowerCase().includes(filter) && <FileItem
-                key={index}
-                icons={icons}
-                index={index}
-                file={file}
-                onCreateFolder={onCreateFolder}
-                onRename={onRename}
-                onFileOpen={onFileOpen}
-                enableFilePreview={enableFilePreview}
-                triggerAction={triggerAction}
-                filesViewRef={filesViewRef}
-                selectedFileIndexes={selectedFileIndexes}
-                handleContextMenu={handleContextMenu}
-                setVisible={setVisible}
-                setLastSelectedFile={setLastSelectedFile}
-                draggable={permissions.move}
-                formatDate={formatDate}
-              />
-            ))}
-          </> 
+        let list = currentPathFiles.filter (f => f.name.toLowerCase().includes(filter))
+        let text=list.length + ' Items'
+        if (selectedFileIndexes.length>0) {
+            text+= '\u00A0\u00A0\u00A0\u00A0' + selectedFileIndexes.length +'\u00A0items selected'
+            let size = list.filter ((f,i) => selectedFileIndexes.includes(i)).reduce((a,c) => {
+                return (c.size||0)+a
+            },0)
+            var units = 'bytes'
+            if (size>1024) {
+                size = (size/1024).toFixed(0)
+                units='KB'
+                if (size >1024) {
+                    size = (size/1024).toFixed(0)
+                    units='MB'
+                }
+            }
+            text+= '\u00A0\u00A0\u00A0\u00A0' + size + '\u00A0' + units
+        }
+
+        return (
+            <div className="statusbar" style={{...(!options.folderTree? {borderBottomLeftRadius: '8px'} : {})}}>{text}</div>
         )
-        :
-        (
-          <div className="empty-folder">{t("folderEmpty")}</div>
-        )}
+    }
 
-        <ContextMenu
-          filesViewRef={filesViewRef}
-          contextMenuRef={contextMenuRef.ref}
-          menuItems={isSelectionCtx ? contextItems : emptySelecCtxItems}
-          visible={visible}
-          setVisible={setVisible}
-          clickPosition={clickPosition}
-        />
-      </div>
-      {renderStatusBar()}
-    </>
+    return (<>
+        <div
+            ref={filesViewRef}
+            className={`files ${activeLayout}`}
+            style={{height: (options.statusBar? 'calc(100% - (54px))':'calc(100% - (35px))'), borderBottom: (options.statusBar?1:0)}}
+            onContextMenu={handleContextMenu}
+            onClick={unselectFiles}
+        >
+            {activeLayout === "list" && (
+                <FilesHeader unselectFiles={unselectFiles} onSort={handleSort} sortConfig={sortConfig} />
+            )}
 
-  )
+            {currentPathFiles?.length > 0 ? (
+                <>
+                    {currentPathFiles.map((file, index) => (
+                    file.name.toLowerCase().includes(filter) && <FileItem
+                        key={index}
+                        icons={icons}
+                        index={index}
+                        file={file}
+                        onCreateFolder={onCreateFolder}
+                        onRename={onRename}
+                        onFileOpen={onFileOpen}
+                        enableFilePreview={enableFilePreview}
+                        triggerAction={triggerAction}
+                        filesViewRef={filesViewRef}
+                        selectedFileIndexes={selectedFileIndexes}
+                        handleContextMenu={handleContextMenu}
+                        setVisible={setVisible}
+                        setLastSelectedFile={setLastSelectedFile}
+                        draggable={permissions.move}
+                        formatDate={formatDate}
+                    />
+                    ))}
+                </> 
+            )
+            :
+            (
+                <div className="empty-folder">{t("folderEmpty")}</div>
+            )}
+
+            <ContextMenu
+                filesViewRef={filesViewRef}
+                contextMenuRef={contextMenuRef.ref}
+                menuItems={isSelectionCtx ? contextItems : emptySelecCtxItems}
+                visible={visible}
+                setVisible={setVisible}
+                clickPosition={clickPosition}
+            />
+        </div>
+        {renderStatusBar()}
+    </>)
 }
 
 FileList.displayName = "FileList";
--- frontend/src/components/SearchInput/SearchInput.jsx
+++ frontend/src/components/SearchInput/SearchInput.jsx
@@ -6,49 +6,48 @@ const SearchInput = ({ onFilterChange }) => {
         if ("Delete Home End".includes(e.key)) e.stopPropagation()
     }
 
-  return (
-    <div>
-        <input 
-            id={id}
-            placeholder='Search...'
-            className='search-input'
-            onFocus={(e) => e.target.placeholder = ""}
-            onBlur={
-                (e) => {
-                    e.target.placeholder = 'Search...'
+    return (
+        <div>
+            <input 
+                id={id}
+                placeholder='Search...'
+                className='search-input'
+                onFocus={(e) => e.target.placeholder = ""}
+                onBlur={
+                    (e) => {
+                        e.target.placeholder = 'Search...'
+                    }
                 }
-            }
-            onKeyDown={handleKeyDown}
-            onKeyUp={(e) => {
-                if (e.key === 'Escape') {
-                    onFilterChange('')
-                    e.preventDefault()
-                    document.getElementById(id).value=''
-                    e.target.placeholder = 'Search...'
-                    e.target.blur()
-                }
-                else {
-                    onFilterChange(document.getElementById(id).value)
-                }
-            }}
-        />
-        <svg 
-            width="16"
-            height="16"
-            fill="none"
-            stroke="#000"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            viewBox="0 0 24 24"
-            style={{ position: "absolute", right: "12px", top: "50%", transform: "translateY(-60%)", pointerEvents: "none" }}
-        >
-            <circle cx="11" cy="11" r="6" />
-            <line x1="21" y1="21" x2="14.65" y2="14.65" />
-        </svg>        
-    </div>
-
-  )
+                onKeyDown={handleKeyDown}
+                onKeyUp={(e) => {
+                    if (e.key === 'Escape') {
+                        onFilterChange('')
+                        e.preventDefault()
+                        document.getElementById(id).value=''
+                        e.target.placeholder = 'Search...'
+                        e.target.blur()
+                    }
+                    else {
+                        onFilterChange(document.getElementById(id).value)
+                    }
+                }}
+            />
+            <svg 
+                width="16"
+                height="16"
+                fill="none"
+                stroke="#000"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+                style={{ position: "absolute", right: "12px", top: "50%", transform: "translateY(-60%)", pointerEvents: "none" }}
+            >
+                <circle cx="11" cy="11" r="6" />
+                <line x1="21" y1="21" x2="14.65" y2="14.65" />
+            </svg>        
+        </div>
+    )
 }
 
 export default SearchInput
